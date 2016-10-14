00154d0ec2c1589b73d460a3042a15cad177f1b9
Samir Ghosh <hello@samir.tech>
2016-10-13 20:02:19 -0700

    wassup leo!:wq

---
+++ src/cell.cpp
@@ -0,0 +1,9 @@
+//
+//  cell.cpp
+//  langston-had-an-ant
+//
+//  Created by dorsalstream on 10/13/16.
+//
+//
+
+#include "cell.hpp"
---
+++ src/cell.hpp
@@ -0,0 +1,14 @@
+//
+//  cell.hpp
+//  langston-had-an-ant
+//
+//  Created by dorsalstream on 10/13/16.
+//
+//
+
+#ifndef cell_hpp
+#define cell_hpp
+
+#include <stdio.h>
+
+#endif /* cell_hpp */
